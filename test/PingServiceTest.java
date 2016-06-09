c847ef990dec7f77512b79a4ec1e87ca5ac8bf3d
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2016-06-10 00:29:47 +0400

    Init ping service

---
+++ test/PingServiceTest.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Nika Doghonadze
+ */
+import ge.edu.freeuni.sdp.sprinkler_switch.core.PingService;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Nika Doghonadze.
+ */
+public class PingServiceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(PingService.class);
+    }
+
+    @Test
+    public void testGetPing() {
+        Response actual = target("ping").request().get();
+        assertEquals(Response.Status.OK.getStatusCode(), actual.getStatus());
+    }
+}

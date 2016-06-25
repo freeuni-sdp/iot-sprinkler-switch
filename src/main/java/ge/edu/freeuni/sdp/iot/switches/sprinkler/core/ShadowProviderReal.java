8b8afec3401d7f3506e8371e89621589f48c979c
Nika Doghonadze <ndogh13@freeuni.edu.ge>
2016-06-25 14:24:07 +0400

    Divide real switch implementation and simulator

---
+++ src/main/java/ge/edu/freeuni/sdp/iot/switches/sprinkler/core/ShadowProviderReal.java
@@ -0,0 +1,59 @@
+package ge.edu.freeuni.sdp.iot.switches.sprinkler.core;
+
+import ge.edu.freeuni.sdp.iot.switches.sprinkler.model.HouseData;
+import ge.edu.freeuni.sdp.iot.switches.sprinkler.model.SwitchCommand;
+import ge.edu.freeuni.sdp.iot.switches.sprinkler.model.SwitchStatus;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Nika Doghonadze
+ */
+public class ShadowProviderReal extends ShadowProvider {
+    private Client client;
+
+    public ShadowProviderReal(HouseRegistry houseRegistry, Client client) {
+        super(houseRegistry);
+        this.client = client;
+    }
+
+    @Override
+    protected SwitchStatus readSwitchStatus(HouseData houseData) {
+        String url = getUrl(houseData);
+        Response response =
+                client.target(url)
+                        .request(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .get();
+
+        if (response.getStatus() != Response.Status.OK.getStatusCode())
+            return null;
+
+        return response.readEntity(SwitchStatus.class);
+    }
+
+    @Override
+    protected SwitchStatus sendSwitchCommand(HouseData houseData, SwitchStatus switchStatus) {
+        String url = getUrl(houseData);
+        SwitchCommand switchCommand = SwitchCommand.fromStatus(switchStatus);
+        Response response =
+                client.target(url)
+                        .request(MediaType.APPLICATION_JSON)
+                        .put(Entity.json(switchCommand.toString()));
+
+        if (response.getStatus() != Response.Status.OK.getStatusCode())
+            return null;
+
+        return response.readEntity(SwitchStatus.class);
+    }
+
+
+    protected String getUrl(HouseData houseData) {
+        return "https://" + houseData.getSwitchIpAddress() +
+                "/webapi/houses/" + houseData.getHouseId() +
+                "/sprinkler-switch";
+    }
+}
--- src/main/java/ge/edu/freeuni/sdp/iot/switches/sprinkler/core/ShadowProviderSimulator.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/switches/sprinkler/core/ShadowProviderSimulator.java
@@ -1,54 +1,23 @@
 package ge.edu.freeuni.sdp.iot.switches.sprinkler.core;
 
 import ge.edu.freeuni.sdp.iot.switches.sprinkler.model.HouseData;
-import ge.edu.freeuni.sdp.iot.switches.sprinkler.model.SwitchCommand;
-import ge.edu.freeuni.sdp.iot.switches.sprinkler.model.SwitchStatus;
 
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 /**
  * Created by Nika Doghonadze
  */
-public class ShadowProviderSimulator extends ShadowProvider {
+public class ShadowProviderSimulator extends ShadowProviderReal {
     private String appUrl;
-    private Client client;
 
     public ShadowProviderSimulator(HouseRegistry houseRegistry, String appUrl, Client client) {
-        super(houseRegistry);
+        super(houseRegistry, client);
         this.appUrl = appUrl;
-        this.client = client;
     }
 
     @Override
-    protected SwitchStatus readSwitchStatus(HouseData houseData) {
-        String url = appUrl + "/webapi/houses/" + houseData.getHouseId() + "/sprinkler-switch";
-        Response response =
-                client.target(url)
-                        .request(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .get();
-
-        if (response.getStatus() != Response.Status.OK.getStatusCode())
-            return null;
-
-        return response.readEntity(SwitchStatus.class);
-    }
-
-    @Override
-    protected SwitchStatus sendSwitchCommand(HouseData houseData, SwitchStatus switchStatus) {
-        String url = appUrl + "/webapi/houses/" + houseData.getHouseId() + "/sprinkler-switch";
-        SwitchCommand switchCommand = SwitchCommand.fromStatus(switchStatus);
-        Response response =
-                client.target(url)
-                .request(MediaType.APPLICATION_JSON)
-                .put(Entity.json(switchCommand.toString()));
-
-        if (response.getStatus() != Response.Status.OK.getStatusCode())
-            return null;
-
-        return response.readEntity(SwitchStatus.class);
+    protected String getUrl(HouseData houseData) {
+        return  appUrl + "/webapi/houses/" + houseData.getHouseId() +
+                "/sprinkler-switch";
     }
 }
